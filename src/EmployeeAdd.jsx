c0d6934c69b77974feb2ccb59af3465d38e17f26
peppamint-png <dhouston7516@sdsu.edu>
2024-05-04 12:45:08 -0700

    add has new fields

--- src/EmployeeAdd.jsx
+++ src/EmployeeAdd.jsx
@@ -3,21 +3,37 @@ import React, { Component } from 'react';
 class EmployeeAdd extends Component {
     state = {
         name: '',
-        title: ''
+        title: '',
+        email: '',
+        extension: '',
+        dateHired: '',
+        currentlyEmployed: false // Assuming this is a boolean
     };
 
     handleInputChange = (event) => {
         const { name, value } = event.target;
-        this.setState({ [name]: value });
+        const newValue = name === 'currentlyEmployed' ? event.target.checked : value;
+        this.setState({ [name]: newValue });
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
         this.props.onEmployeeAdd({
             name: this.state.name,
-            title: this.state.title
+            title: this.state.title,
+            email: this.state.email,
+            extension: this.state.extension,
+            dateHired: this.state.dateHired,
+            currentlyEmployed: this.state.currentlyEmployed
         });
-        this.setState({ name: '', title: '' }); // Reset form fields
+        this.setState({ 
+            name: '', 
+            title: '', 
+            email: '', 
+            extension: '', 
+            dateHired: '',
+            currentlyEmployed: false
+        }); // Reset form fields
     };
 
     render() {
@@ -37,6 +53,36 @@ class EmployeeAdd extends Component {
                     onChange={this.handleInputChange}
                     placeholder="Title"
                 />
+                <input
+                    type="text"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    placeholder="Email"
+                />
+                <input
+                    type="text"
+                    name="extension"
+                    value={this.state.extension}
+                    onChange={this.handleInputChange}
+                    placeholder="Extension"
+                />
+                <input
+                    type="date"
+                    name="dateHired"
+                    value={this.state.dateHired}
+                    onChange={this.handleInputChange}
+                    placeholder="Date Hired"
+                />
+                <label>
+                    Currently Employed:
+                    <input
+                        type="checkbox"
+                        name="currentlyEmployed"
+                        checked={this.state.currentlyEmployed}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
                 <button type="submit">Add Employee</button>
             </form>
         );
